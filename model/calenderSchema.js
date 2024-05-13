cc17496556c155c9ede1fec37eda6cd80b1ea6da
Dharmesh Shimpi <dharmesh.shimpi@gmail.com>
2024-05-14 00:25:21 +0530

    .

--- model/calenderSchema.js
+++ model/calenderSchema.js
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const calendarSchema = new Schema({
-	date: Number,
-	month: String,
-	year: Number,
-	status: {
-		type: String,
-		enum: ['Nothing', 'Done', 'Not Done'],
-		default: 'Nothing',
+const calendarSchema = new Schema(
+	{
+		date: Number,
+		month: String,
+		year: Number,
+		status: {
+			type: String,
+			enum: ['Nothing', 'Done', 'Not Done'],
+			default: 'Nothing',
+		},
 	},
-}, { timestamps: true });
+	{ timestamps: true },
+);
 
 export const Calendar = mongoose.model('Calendar', calendarSchema);
-
-
--- server.js
+++ server.js
@@ -4,13 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 async function connectDatabase() {
 	try {
-		await mongoose.connect(
-			process.env.MONGO_URI,
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			},
-		);
+		await mongoose.connect(process.env.MONGO_URI, {
+			serverSelectionTimeoutMS: 60000,
+		});
 	} catch (error) {
 		console.log(error);
 	}
