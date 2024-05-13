106c7a6669ecdb248c79ab715089b202a864ccd2
Dharmesh Shimpi <dharmesh.shimpi@gmail.com>
2024-05-13 23:59:07 +0530

    .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config();
 async function connectDatabase() {
 	try {
 		await mongoose.connect(
-			process.env.MONGO_URL,
+			process.env.MONGO_URI,
 			{
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
